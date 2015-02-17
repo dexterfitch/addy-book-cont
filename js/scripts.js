e7fb4b41774090e1960d7571ffed4e83bb91a5f3
Luke Chinworth & Dex Fitch <student@epicodus.com>
2015-02-17 10:10:48 -0800

    add address object linked to each contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,51 @@
+var fullAddress
+
 $(document).ready(function() {
   $('form#new-contact').submit(function(event) {
     event.preventDefault();
 
     var inputFirstName = $('input#new-first-name').val();
     var inputLastName = $('input#new-last-name').val();
-    var inputAddress = $('input#new-address').val();
-    var newContact = { firstName: inputFirstName, lastName: inputLastName, address: inputAddress };
+    var inputStreet = $('input#new-street').val();
+    var inputCity = $('input#new-city').val();
+    var inputState = $('input#new-state').val();
+    var inputZip = $('input#new-zip').val();
+
+    var newAddress = { street: inputStreet,
+                       city: inputCity,
+                       state: inputState,
+                       zip: inputZip,
+                       streetAddress: function() {
+                         return this.street;
+                       },
+                       cityStateZipAddress: function() {
+                         return this.city + ", " + this.state + " " + this.zip;
+                       }
+                     };
+    var newContact = { firstName: inputFirstName,
+                       lastName: inputLastName,
+                       streetAddress: newAddress.streetAddress(),
+                       cityStateZipAddress: newAddress.cityStateZipAddress()
+                       };
 
     $('ul#contacts').append('<li><span class="contact">' + newContact.firstName + " " + newContact.lastName + '</span></li>');
 
     $('input#new-first-name').val("");
     $('input#new-last-name').val("");
-    $('input#new-address').val("");
+    $('input#new-street').val("");
+    $('input#new-city').val("");
+    $('input#new-state').val("");
+    $('input#new-zip').val("");
+
 
     $('.contact').last().click(function() {
       $('#show-contact').show();
       $('#show-contact h2').text(newContact.firstName + " " + newContact.lastName);
       $('.first-name').text(newContact.firstName);
       $('.last-name').text(newContact.lastName);
-      $('.address').text(newContact.address);
+      $('.street').text(newContact.streetAddress);
+      $('.city-state-zip').text(newContact.cityStateZipAddress);
+
     });
   });
 });
